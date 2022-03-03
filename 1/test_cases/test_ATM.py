994cd94ea2282d0787842da6e264564d017e67cf
anadeg <degterevastasa@gmail.com>
2022-03-03 10:24:46 +0300

    add tests

--- 1/test_cases/test_ATM.py
+++ 1/test_cases/test_ATM.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from classes.ATM import ATM, Banknote
+from classes.ATM import ATM, Banknote, Card, Bank
+from exceptions.CardIsInserted import CardIsInsertedException
 
 
 class TestATM(TestCase):
@@ -19,15 +20,44 @@ class TestATM(TestCase):
         atm.add_money_to_cash_holder(money)
         self.assertEqual(atm.cash_holder, money)
 
-    # def test_insert_card(self):
-    #     self.fail()
-    #
-    # def test_check_card(self):
-    #     self.fail()
-    #
+    def test_insert_card_1(self):
+        atm = ATM()
+        c1 = Card("1111", "me", 1111, {})
+        self.assertEqual(atm.insert_card(c1), "your card was successfully inserted")
+
+    def test_insert_card_2(self):
+        atm = ATM()
+        c1 = Card("1111", "me", 1111, {})
+        atm.inserted_card = c1
+        c2 = Card("2222", "not me", 2222, {})
+        e = CardIsInsertedException()
+        self.assertEqual(atm.insert_card(c2), e.massage)
+
+    def test_check_card_1(self):
+        atm = ATM()
+        self.assertFalse(atm.check_card())
+
+    def test_check_card_2(self):
+        b1 = Bank()
+        c1 = Card("1111", "me", 1111, {})
+        c2 = Card("2222", "not me", 2222, {})
+        b1._Bank__cards = [c1, c2]
+        atm = ATM()
+        atm.insert_card(c1)
+        self.assertTrue(atm.check_card())
+
     # def test_withdraw_money(self):
     #     self.fail()
-    #
+
+    def test_withdraw_money_1(self):
+        b1 = Bank()
+        c1 = Card("1111", "me", 1111, {})
+        c2 = Card("2222", "not me", 2222, {})
+        b1._Bank__cards = [c1]
+        atm = ATM()
+        atm.insert_card(c2)
+        self.assertFalse(atm.withdraw_money("USD", 10))
+
     # def test_show_balance(self):
     #     self.fail()
     #
