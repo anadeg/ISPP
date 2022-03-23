fb4f4fa3c4d4103d451460f56bb9a33f47c7f099
anadeg <degterevastasa@gmail.com>
2022-03-23 15:07:56 +0300

    rework endElement

--- 2/xmls/sax_reader.py
+++ 2/xmls/sax_reader.py
@@ -2,10 +2,15 @@ import xml.sax
 
 
 class StudentHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        super().__init__()
+        self.student_table = []
+        self.student_data = {}
+
     def startElement(self, name, attrs):
         self.current = name
-        # if name == "student":
-        #     print(f"======== STUDENT {attrs['id']} ========")
+        if name == "student":
+            pass
 
     def characters(self, content):
         if self.current == "name":
@@ -21,20 +26,18 @@ class StudentHandler(xml.sax.ContentHandler):
 
     def endElement(self, name):
         if self.current == "name":
-            print(f"-- name: {self.name} --")
+            self.student_data["name"] = self.name
         elif self.current == "group":
-            print(f"-- group: {self.group} --")
+            self.student_data["group"] = self.group
         elif self.current == "sick":
-            print(f"-- sick time: {self.sick} --")
+            self.student_data["sick"] = self.sick
         elif self.current == "absence":
-            print(f"-- absence time: {self.absence} --")
+            self.student_data["absence"] = self.absence
         elif self.current == "other":
-            print(f"-- other time: {self.other} --")
+            self.student_data["other"] = self.other
+
+        self.student_table.append(self.student_data)
+        self.student_data = {}
 
         self.current = ""
 
-
-handler = StudentHandler()
-parser = xml.sax.make_parser()
-parser.setContentHandler(handler)
-parser.parse('data0.xml')
