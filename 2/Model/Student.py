d4206cb832e76882c4a5e0b79d0bf383a1246832
anadeg <degterevastasa@gmail.com>
2022-03-24 15:38:12 +0300

    add __str__ method fix __lt__ method

--- 2/Model/Student.py
+++ 2/Model/Student.py
@@ -10,7 +10,14 @@ class Student:
         return self.name > other.name
 
     def __lt__(self, other):
-        return not __gt__(other)
+        return not self.__gt__(other)
 
     def __eq__(self, other):
         return self.name == other.name
+
+    def __str__(self):
+        return f"\nname: {self.name}," \
+               f"\ngroup: {self.group}," \
+               f"\nsick: {self.sick}," \
+               f"\nabsence: {self.absence}" \
+               f"\nother: {self.other}"
