87c8ae0646b60cb45127f63a138a2a48fe9e0bb2
anadeg <degterevastasa@gmail.com>
2022-04-20 09:19:48 +0300

    added screens to main view file

--- 2/View/view.py
+++ 2/View/view.py
@@ -1,18 +1,51 @@
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
+from kivymd.uix.button import MDRectangleFlatButton
 
-from view_ui import text_ui
+from view_ui import text_ui, enter_path, enter_path_button, path_dialog
+from screens_ui import screens
+
+
+class ShowStudentsScreen(Screen):
+    pass
+
+
+class InputPathScreen(Screen):
+    pass
+
+
+sm = ScreenManager()
+sm.add_widget(ShowStudentsScreen(name='table'))
+sm.add_widget(InputPathScreen(name='input path'))
 
 
 class StudentApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = "Green"
-        screen = Builder.load_string(text_ui)
+        # self.screen = Screen()
+        #
+        # self.theme_cls.primary_palette = "Green"
+        # # self.entering_button = MDRectangleFlatButton(
+        # #                                             text="show",
+        # #                                             pos_hint={"center_x": 0.5, "center_y": 0.4},
+        # #                                             on_release=self.show_path
+        # #                                             )
+        # self.enter_path_button = Builder.load_string(enter_path_button)
+        # self.enter_path = Builder.load_string(enter_path)
+        #
+        # self.screen.add_widget(self.enter_path)
+        # self.screen.add_widget(self.enter_path_button)
+        # return self.screen
+        screen = Builder.load_string(screens)
         return screen
 
+    def show_path(self, obj):
+        input_path_text = self.enter_path.text
+        self.dialog = Builder.load_string(path_dialog)
+        self.screen.add_widget(self.dialog)
+        self.dialog.open()
 
-    def navigation_draw(self):
-        print("shiiiiit")
-
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
 
 StudentApp().run()
