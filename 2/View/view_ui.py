e660b38817bdcd38080c46de0536f64aa4c48e65
anadeg <degterevastasa@gmail.com>
2022-04-20 09:21:41 +0300

    created some new things

--- 2/View/view_ui.py
+++ 2/View/view_ui.py
@@ -45,3 +45,32 @@ MDScreen:
                 
     
 """
+
+enter_path = """
+MDTextField:
+    hint_text: "enter file name"
+    helper_text: "like data.txt, data0.xml and so on"
+    helper_text_mode: "on_focus"
+    pos_hint: {"center_x": 0.5, "center_y": 0.5}
+    size_hint_x: 0.5
+    icon_right: "file-document-multiple"
+"""
+
+enter_path_button = """
+MDScreen:
+    MDRectangleFlatButton:
+        text: "show"
+        pos_hint: {"center_x": 0.5, "center_y": 0.4}
+        on_release: app.show_path
+"""
+
+path_dialog = """
+MDDialog:
+    title: "path input"
+    text: app.input_path_text
+    MDFlatButton:
+        text: "close"
+        on_release: app.close_dialog
+    MDFlatButton:
+        text: "more"
+"""
