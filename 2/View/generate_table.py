d2495052df16a8b821d06a9989dbb54360c9660a
anadeg <degterevastasa@gmail.com>
2022-04-21 15:54:52 +0300

    created students generating function (for test purposes)

---
+++ 2/View/generate_table.py
@@ -0,0 +1,27 @@
+from random import choice, randint
+
+
+def generate_table(number_of_rows):
+    names = ["Liam", "Olivia", "Noah", "Emma",
+             "Oliver", "Ava", "Elijah", "Charlotte",
+             "William", "Sophia", "James", "Amelia",
+             "Eric", "Kyle", "Kenny", "Stenley"]
+
+    last_names = ["Smith", "Jonhson", "Jones", "Brown",
+                  "Garcia", "Miller", "Davis", "Taylor",
+                  "Cartman", "Broflovski", "McCormick", "Marsh",
+                  "Lee", "King", "Scott", "Stewart"
+                  ]
+    res = []
+
+    for _ in range(number_of_rows):
+        name = choice(names)
+        last_name = choice(last_names)
+        full_name = " ".join([name, last_name])
+        group = randint(10, 90)
+        sick = randint(0, 100)
+        absent = randint(0, 100)
+        other = randint(0, 100)
+        res.append([full_name, group, sick, absent, other])
+
+    return res
