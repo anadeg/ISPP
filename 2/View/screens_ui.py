ed6ad9a7f1bb77f073b2111880333303cf39d5c6
anadeg <degterevastasa@gmail.com>
2022-04-21 15:56:38 +0300

    made text field (for dialog box) main page with navigation

--- 2/View/screens_ui.py
+++ 2/View/screens_ui.py
@@ -1,59 +1,17 @@
-screens = """
-MDNavigationLayout:
-    MDNavigationDrawer:
-        NavigationDrawerIconButton:
-            text: "input path"
-            on_release: screen_manager.current = 'input_screen'
-        NavigationDrawerIconButton:
-            text: "show table"
-            on_release: screen_manager.current = 'table_screen'
-                
-    ScreenManager:
-        id: "screen_manager"
-        InputPathScreen:
-        ShowStudentsScreen:
-        
-<ShowStudentsScreen>:
-    name: 'table_screen'
-    MDRectangleFlatButton:
-        text: 'go to me'
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-            
-<InputPathScreen>:
-    name: 'input_screen'
+content_of_entering_path = """
+<Content>:
+    orientation: 'vertical'
+    spacing: '8dp'
+    size_hint_y :None
+    height: '40dp'
+    
     MDTextField:
-        hint_text: "enter file name"
-        helper_text: "like data.txt, data0.xml and so on"
-        helper_text_mode: "on_focus"
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-        size_hint_x: 0.5
-        icon_right: "file-document-multiple"
-    MDRectangleFlatButton:
-        text: "go to table"
-        pos_hint: {"center_x": 0.5, "center_y": 0.4}
-        on_release: root.manager.current = 'table'
+        hint_text: 'enter file name'
+        helper_text: 'file must be in folder "xmls"'
+        helper_text_mode: 'on_focus'
 """
 
-
-screens2 = """
-<ContentNavigationDrawer>:
-    ScrollView:
-        MDList:
-            OneLineIconListItem:
-                text: 'enter path'
-                on_press:
-                    root.drawer.set_state("close")
-                    root.screen_manager.current = 'input_screen'
-                IconLeftWidget:
-                    icon: 'file-multiple'
-            OneLineIconListItem:
-                text: 'table'
-                on_press:
-                    root.drawer.set_state("close")
-                    root.screen_manager.current = 'table_screen'
-                IconLeftWidget:
-                    icon: 'file-table'
-                    
+using_navigation = """
 Screen:
     MDToolbar:
         id: toolbar
@@ -62,38 +20,23 @@ Screen:
         title: "menu"
         left_action_items: [["menu", lambda x: drawer.set_state("open")]]
         
-    MDNavigationLayout:
-        x: toolbar.height
-        ScreenManager:
-            id: screen_manager
-            InputPathScreen:
-            ShowStudentsScreen:
-            
+        MDNavigationLayout:
+            x: toolbar.height
+        
     MDNavigationDrawer:
         id: drawer
         ContentNavigationDrawer:
-            screen_manager: screen_manager
             drawer: drawer
-            
-            
 
-<ShowStudentsScreen>:
-    name: 'table_screen'
-    MDRectangleFlatButton:
-        text: 'go to me'
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-            
-<InputPathScreen>:
-    name: 'input_screen'
-    MDTextField:
-        hint_text: "enter file name"
-        helper_text: "like data.txt, data0.xml and so on"
-        helper_text_mode: "on_focus"
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-        size_hint_x: 0.5
-        icon_right: "file-document-multiple"
-    MDRectangleFlatButton:
-        text: "go to table"
-        pos_hint: {"center_x": 0.5, "center_y": 0.4}
-        on_release: root.manager.current = 'table_screen'
+<ContentNavigationDrawer>:
+    ScrollView:
+        MDList:
+            OneLineIconListItem:
+                text: "enter path"
+                on_press: 
+                    root.drawer.set_state("close")
+                    app.open_file_name_dialog()
+                IconLeftWidget:
+                    icon: 'file-multiple'
+                    
 """
