1d84ff70a0c2dd915977ff7348e89cfacb4652d1
anadeg <degterevastasa@gmail.com>
2022-04-25 17:09:42 +0300

    str fields

--- 2/View/generate_table.py
+++ 2/View/generate_table.py
@@ -1,6 +1,8 @@
 from random import choice, randint
 import bisect
 
+from Model.student import Student
+
 
 def generate_table(number_of_rows):
     names = ["Liam", "Olivia", "Noah", "Emma",
@@ -19,10 +21,9 @@ def generate_table(number_of_rows):
         name = choice(names)
         last_name = choice(last_names)
         full_name = " ".join([name, last_name])
-        group = randint(10, 20)
-        sick = randint(0, 100)
-        absent = randint(0, 100)
-        other = randint(0, 100)
-        # res.append([full_name, group, sick, absent, other])
+        group = "".join((str(randint(0, 4)), str(randint(1, 9))))
+        sick = str(randint(0, 100))
+        absent = str(randint(0, 100))
+        other = str(randint(0, 100))
         bisect.insort(res, [full_name, group, sick, absent, other])
     return res
