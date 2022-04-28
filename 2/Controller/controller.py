7f41e81187f0f9a1fd5f1493e17f270d79a8db75
anadeg <degterevastasa@gmail.com>
2022-04-28 08:06:33 +0300

    returned function that worked with files

--- 2/Controller/controller.py
+++ 2/Controller/controller.py
@@ -36,14 +36,15 @@ class Controller:
     def delete_students(self, black_list):
         self.model.delete_students_from_table(black_list)
 
-    # def add_student_to_file(self, file_name, name, group, sick, absent, other):
-    #     try:
-    #         self.model.write_data_in_xml(file_name, [name, group, str(sick), str(absent), str(other)])
-    #     except ValueError:
-    #         self.create_new_file(file_name)
-    #         self.model.write_data_in_xml(file_name, [name, group, str(sick), str(absent), str(other)])
-    #
-    # def create_new_file(self, file_name):
-    #     path = self.model.get_path_to_file(file_name)
-    #     with open(path, 'w') as new_file:
-    #         new_file.write('<?xml version="1.0" ?>\n<table>\n</table>')
+    def add_student_to_file(self, file_name, name, group, sick, absent, other):
+        # path = self.model.get_path_to_file(file_name)
+        try:
+            self.model.write_data_in_xml(file_name, [name, group, str(sick), str(absent), str(other)])
+        except ValueError:
+            self.create_new_file(file_name)
+            self.model.write_data_in_xml(file_name, [name, group, str(sick), str(absent), str(other)])
+
+    def create_new_file(self, file_name):
+        path = self.model.get_path_to_file(file_name)
+        with open(path, 'w') as new_file:
+            new_file.write('<?xml version="1.0" ?>\n<table>\n</table>')
